543a859db8a351096907382a10d1214cb16980a1
rulelau <863915498@qq.com>
2020-04-16 00:23:06 +0800

    嵌套数组

---
+++ src/com/rule/problem/array/ArrayNesting.java
@@ -0,0 +1,53 @@
+package com.rule.problem.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArrayNesting {
+    /**
+     * 超时
+     */
+    public static int arrayNesting(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            Map<Integer, Integer> map = new HashMap();
+            max = Math.max(max, deepTh(map, nums, i));
+        }
+        return max;
+    }
+
+    private static int deepTh(Map<Integer, Integer> map, int[] nums, int index) {
+        int key = nums[index];
+        if (map.containsKey(key)) {
+            map.put(key, map.get(key) + 1);
+        }else {
+            map.put(key, 1);
+        }
+        if (map.get(key) == 2) {
+            return map.size();
+        }
+        return deepTh(map, nums, key);
+    }
+
+    public int arrayNesting2(int[] nums) {
+        boolean[] visited = new boolean[nums.length];
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (!visited[i]) {
+                int start = nums[i], count = 0;
+                do {
+                    start = nums[start];
+                    count++;
+                    visited[start] = true;
+                }
+                while (start != nums[i]);
+                res = Math.max(res, count);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(arrayNesting(new int[]{0, 1}));
+    }
+}
--- src/com/rule/problem/array/FindShortestSubArray.java
+++ src/com/rule/problem/array/FindShortestSubArray.java
@@ -1,34 +1,45 @@
 package com.rule.problem.array;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class FindShortestSubArray {
     /**
-     *  697. 数组的度
+     * 697. 数组的度
      */
-    public int findShortestSubArray(int[] nums) {
+    public static int findShortestSubArray(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
         Map<Integer, List<Integer>> listMap = new HashMap<>();
         int max = 0;
         for (int i = 0; i < nums.length; i++) {
+            List<Integer> list = listMap.getOrDefault(nums[i], new ArrayList<>());
             if (map.containsKey(nums[i])) {
                 int value = map.get(nums[i]) + 1;
                 map.put(nums[i], value);
                 max = Math.max(max, value);
-                List<Integer> list = listMap.get(max);
-                list.add(nums[i]);
-                listMap.put(max, list);
-            }else {
+            } else {
                 map.put(nums[i], 1);
             }
+            list.add(i);
+            listMap.put(nums[i], list);
         }
+        int finalMax = max;
+        AtomicInteger len = new AtomicInteger(50000);
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            Integer k = entry.getKey();
+            Integer v = entry.getValue();
+            if (v == finalMax) {
+                List<Integer> list = listMap.get(k);
+                len.set(Math.min(len.get(), list.get(list.size() - 1) - list.get(0)));
+            }
+        }
+        return len.get() == 50000 ? 1 : len.get() + 1;
+    }
 
-
-
-
-
-        return 0;
+    public static void main(String[] args) {
+        System.out.println(findShortestSubArray(new int[]{1}));
     }
 }
