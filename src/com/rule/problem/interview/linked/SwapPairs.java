e5b4a43b6c6b4da9623f780c10f61b95c3327590
rulelau <863915498@qq.com>
2020-05-09 22:58:23 +0800

    节点交换

---
+++ src/com/rule/problem/interview/linked/SwapPairs.java
@@ -0,0 +1,68 @@
+package com.rule.problem.interview.linked;
+
+/**
+ * 24. 两两交换链表中的节点
+ */
+public class SwapPairs {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static ListNode swapPairs(ListNode head) {
+        // If the list has no node or has only one node left.
+        if ((head == null) || (head.next == null)) {
+            return head;
+        }
+
+        // Nodes to be swapped
+        ListNode secondNode = head.next;
+
+        // Swapping
+        head.next = swapPairs(secondNode.next);
+        secondNode.next = head;
+
+        // Now the head is the second node
+        return secondNode;
+    }
+
+    public static ListNode swapPairs2(ListNode head) {
+        // Dummy node acts as the prevNode for the head node
+        // of the list and hence stores pointer to the head node.
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode prevNode = dummy;
+
+        while (head != null && head.next != null) {
+
+            // Nodes to be swapped
+            ListNode firstNode = head;
+            ListNode secondNode = head.next;
+
+            // Swapping
+            prevNode.next = secondNode;
+            firstNode.next = secondNode.next;
+            secondNode.next = firstNode;
+
+            // Reinitializing the head and prevNode for next swap
+            prevNode = firstNode;
+            head = firstNode.next; // jump
+        }
+
+        // Return the new head node.
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        swapPairs(head);
+    }
+}
