545e68aed010077e2b6fcf33ef6165c276339e87
liurui-1 <liurui-1@thunisoft.com>
2020-07-09 17:32:05 +0800

    86. 分隔链表

---
+++ src/com/rule/problem/interview/linked/Partition.java
@@ -0,0 +1,13 @@
+package com.rule.problem.interview.linked;
+
+/**
+ * 86. 分隔链表
+ */
+public class Partition {
+
+    //输入: head = 1->4->3->2->5->2, x = 3
+    //输出: 1->2->2->4->3->5
+    public ListNode partition(ListNode head, int x) {
+        return null;
+    }
+}
