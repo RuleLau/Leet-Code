2581f67d96fc84a7123963e45f0352913d9f7660
liurui-1 <liurui-1@thunisoft.com>
2020-07-15 20:34:08 +0800

    154. 寻找旋转排序数组中的最小值II

---
+++ src/com/rule/problem/interview/binarySearch/FindMin2.java
@@ -0,0 +1,35 @@
+package com.rule.problem.interview.binarySearch;
+
+public class FindMin2 {
+
+    public static int findMin(int[] nums) {
+        if (nums.length == 2) {
+            return Math.min(nums[0], nums[1]);
+        }
+        int len = nums.length;
+        int left = 0;
+        int right = len - 1;
+        int mid = (left + right) / 2;
+        while (left < right) {
+            if (right - left == 1) {
+                return Math.min(nums[left], nums[right]);
+            }else if (nums[mid] >= nums[left] && nums[mid] >= nums[right]) {
+                left = mid + 1;
+            } else if (nums[mid] <= nums[left] && nums[mid] <= nums[right]) {
+                if (right - left == 2) {
+                    break;
+                }
+                right = mid + 1;
+            } else if (nums[mid] >= nums[left] && nums[mid] <= nums[right]) {
+                right = mid - 1;
+            }
+            mid = (left + right) / 2;
+        }
+        return nums[mid];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findMin(new int[]{1, 3, 5}));
+        System.out.println(findMin(new int[]{2, 2, 2, 0, 1}));
+    }
+}
