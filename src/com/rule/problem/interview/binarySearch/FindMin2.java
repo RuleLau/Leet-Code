3e8c93c8665bec8fc1094ca608de21db4f72ce1c
liurui-1 <liurui-1@thunisoft.com>
2020-07-16 11:44:38 +0800

    154. 寻找旋转排序数组中的最小值II

--- src/com/rule/problem/interview/binarySearch/FindMin2.java
+++ src/com/rule/problem/interview/binarySearch/FindMin2.java
@@ -2,6 +2,7 @@ package com.rule.problem.interview.binarySearch;
 
 public class FindMin2 {
 
+    //[3,3,3,3,3,3,3,3,1,3]
     public static int findMin(int[] nums) {
         if (nums.length == 2) {
             return Math.min(nums[0], nums[1]);
@@ -13,7 +14,9 @@ public class FindMin2 {
         while (left < right) {
             if (right - left == 1) {
                 return Math.min(nums[left], nums[right]);
-            }else if (nums[mid] >= nums[left] && nums[mid] >= nums[right]) {
+            } else if (nums[mid] == nums[right] && nums[mid] == nums[left]) {
+                left = left + 1;
+            } else if (nums[mid] >= nums[left] && nums[mid] > nums[right]) {
                 left = mid + 1;
             } else if (nums[mid] <= nums[left] && nums[mid] <= nums[right]) {
                 if (right - left == 2) {
@@ -31,5 +34,10 @@ public class FindMin2 {
     public static void main(String[] args) {
         System.out.println(findMin(new int[]{1, 3, 5}));
         System.out.println(findMin(new int[]{2, 2, 2, 0, 1}));
+        System.out.println(findMin(new int[]{3, 3, 3, 3, 3, 3, 3, 3, 1, 3}));
+        System.out.println(findMin(new int[]{1, 3, 3}));
+        System.out.println(findMin(new int[]{3, 1, 1}));
+        System.out.println(findMin(new int[]{10,1,10,10,10}));
+        System.out.println(findMin(new int[]{2,2,2,0,1}));
     }
 }
