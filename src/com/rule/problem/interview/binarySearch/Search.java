54fea52e6f73da170e2709594abbdfd3bc2779b6
liurui-1 <liurui-1@thunisoft.com>
2020-07-20 18:33:37 +0800

    33. 搜索旋转排序数组-2

--- src/com/rule/problem/interview/binarySearch/Search.java
+++ src/com/rule/problem/interview/binarySearch/Search.java
@@ -9,29 +9,30 @@ public class Search {
         int len = nums.length;
         int left = 0;
         int right = len - 1;
-        while (left < right) {
+        while (left <= right) {
             int mid = (left + right) / 2;
             if (nums[mid] == target) {
                 return mid;
             }
-            if (nums[mid] < target) {
-                if (target > nums[right]) {
-                    right = mid - 1;
-                }else {
+            if (nums[mid] > nums[left] && nums[mid] > nums[right]) {
+                if (target > nums[mid] || target < nums[right]) {
                     left = mid + 1;
+                }else if (target < nums[mid]){
+                    right = mid - 1;
                 }
-            }else {
-                if (target <= nums[right]) {
-                    right = mid - 1;
-                }else {
+            }else if (nums[mid] > nums[left] && nums[mid] < nums[right]) {
+                if (target > nums[mid]) {
                     left = mid + 1;
+                }else {
+                    right = mid - 1;
                 }
             }
+
         }
         return -1;
     }
 
     public static void main(String[] args) {
-        System.out.println(search(new int[]{4,5,6,7,0,1,2}, 0));
+        System.out.println(search(new int[]{4,5,6,7,0,1,2}, 2));
     }
 }
