57e772b4d1963f50c2ad255c92accf819b90f92a
liurui1 <liurui1@sinog2c.com>
2020-12-01 20:58:57 +0800

    week215

--- src/com/rule/problem/week/Week217.java
+++ src/com/rule/problem/week/Week217.java
@@ -1,9 +1,6 @@
 package com.rule.problem.week;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * Week217
@@ -15,28 +12,28 @@ import java.util.Queue;
 public class Week217 {
 
     public int[] mostCompetitive(int[] nums, int k) {
-        int n = nums.length;
-        int[] res = new int[k];
-        Deque<Integer> queue = new ArrayDeque<>();
-        int index = 0;
-        int len = n - k + 1;
-        for (int i = 0; i < n; i++) {
-            //队头滑出
-            if (queue.size() > 0 && (queue.peekFirst() < i - len + 1)) {
-                queue.pollFirst();
+        Stack<Integer> stack = new Stack<>();
+        stack.add(-1);
+        int len = nums.length;
+
+        for (int i = 0; i < len; i++) {
+            //当前元素比队尾元素小，将队尾元素出栈
+            //此处需要另外判断数组剩余长度够不够填满栈，不然最后答案长度可能会小于k
+            while (nums[i] < stack.peek() && k - stack.size() + 1 < len - i) {
+                stack.pop();
             }
-            //与新进来元素比较，维护单调性
-            while (queue.size() > 0 && nums[queue.peekLast()] > nums[i]) {
-                queue.pollLast();
-            }
-            //新元素入队
-            queue.addLast(i);
-            //更新答案
-            if (i >= len - 1) {
-                res[index++] = nums[queue.pollFirst()];
+            if (stack.size() < k + 1) {
+                stack.add(nums[i]);
             }
         }
-        return res;
+
+        int[] ret = new int[k];
+
+        while (k > 0) {
+            ret[--k] = stack.pop();
+        }
+
+        return ret;
     }
 
 }
