b929b0c85d8071fa4262441ec7f5092f4fd55c8d
rulelau <863915498@qq.com>
2020-12-17 22:08:41 +0800

    每日一题

--- src/com/rule/problem/day/MonotoneIncreasingDigits.java
+++ src/com/rule/problem/day/MonotoneIncreasingDigits.java
@@ -14,38 +14,42 @@ public class MonotoneIncreasingDigits {
         char[] chars = s.toCharArray();
         StringBuilder ans = new StringBuilder();
         int flag = 1;
+        int len = chars.length;
         Deque<Integer> stack = new ArrayDeque<>();
-        stack.add(Character.getNumericValue(chars[0]));
+        stack.push(Character.getNumericValue(chars[0]));
         for (int i = 1; i < chars.length; i++) {
             int a = Character.getNumericValue(chars[i]);
             if (flag == 1) {
                 if (a >= stack.peek()) {
-                    stack.add(a);
+                    stack.push(a);
                 } else {
                     int size = stack.size();
-                    while (!stack.isEmpty()) {
-                        Integer peek = stack.peek();
-
+                    a = stack.pop();
+                    while (!stack.isEmpty() && a == stack.peek()) {
+                        a = stack.pop();
                     }
                     stack.push(a - 1);
-                    for (int j = 0; j < size - stack.size(); j++) {
-                        stack.add(9);
+                    for (int j = 0; j <= size - stack.size() + 1; j++) {
+                        stack.push(9);
                     }
                     flag = 0;
                 }
             } else {
-                stack.add(9);
+                stack.push(9);
             }
+        }
+        while (stack.size() < len) {
+            stack.push(9);
         }
 
         while (!stack.isEmpty()) {
-            ans.append(stack.poll());
+            ans.append(stack.pollLast());
         }
         return Integer.parseInt(ans.toString());
     }
 
     public static void main(String[] args) {
-        System.out.println(monotoneIncreasingDigits(668841));
+        System.out.println(monotoneIncreasingDigits(9999998));
     }
 
 }
---
+++ src/com/rule/problem/stone/StoneGame.java
@@ -0,0 +1,20 @@
+package com.rule.problem.stone;
+
+import java.util.Arrays;
+
+public class StoneGame {
+
+    public boolean stoneGame(int[] piles) {
+        int len = piles.length;
+        int[][] dp = new int[len][len];
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = piles[i];
+        }
+        for (int i = len - 2; i >= 0; i--) {
+            for (int j = i + 1; j < len; j++) {
+                dp[j][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][len - 1] > 0;
+    }
+}
