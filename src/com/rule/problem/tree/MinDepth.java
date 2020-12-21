2809279bb2cb10f2a2260fce4f824f859b987f86
rulelau <863915498@qq.com>
2020-12-21 23:12:02 +0800

    树

--- src/com/rule/problem/tree/MinDepth.java
+++ src/com/rule/problem/tree/MinDepth.java
@@ -1,5 +1,8 @@
 package com.rule.problem.tree;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class MinDepth {
 
     public int minDepth(TreeNode root) {
@@ -18,4 +21,37 @@ public class MinDepth {
         }
         return min + 1;
     }
+
+    /**
+     * bfs
+     * @param root
+     * @return
+     */
+    public int minDepthII(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        int depth = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                if (poll.left == null && poll.right == null) {
+                    return depth;
+                }
+                if (poll.left != null) {
+                    queue.add(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.add(poll.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+
+
 }
