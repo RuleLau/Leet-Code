49e0384d1dfa2c5172068a566d3a8e1d7d5ac196
liurui1 <liurui1@sinog2c.com>
2020-12-21 20:43:23 +0800

    树

---
+++ src/com/rule/problem/tree/MinDepth.java
@@ -0,0 +1,21 @@
+package com.rule.problem.tree;
+
+public class MinDepth {
+
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        int min = Integer.MAX_VALUE;
+        if (root.left != null) {
+            min = Math.min(minDepth(root.left), min);
+        }
+        if (root.right != null) {
+            min = Math.min(min, minDepth(root.right));
+        }
+        return min + 1;
+    }
+}
