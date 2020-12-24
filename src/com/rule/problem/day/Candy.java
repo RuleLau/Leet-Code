5e807570f2a2181f5d970f91cffa8ac8549fed83
liurui1 <liurui1@sinog2c.com>
2020-12-24 20:54:33 +0800

    每日一题

---
+++ src/com/rule/problem/day/Candy.java
@@ -0,0 +1,30 @@
+package com.rule.problem.day;
+
+import java.util.Arrays;
+
+/**
+ * 135. 分发糖果
+ */
+public class Candy {
+
+    public int candy(int[] ratings) {
+        int[] left = new int[ratings.length];
+        int[] right = new int[ratings.length];
+        Arrays.fill(left, 1);
+        Arrays.fill(right, 1);
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            }
+        }
+        int count = left[ratings.length - 1];
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                right[i] = right[i + 1] + 1;
+            }
+            count += Math.max(left[i], right[i]);
+        }
+        return count;
+    }
+
+}
